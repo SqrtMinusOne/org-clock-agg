7ccf1521c6556d68fc69a62d978d5d53ed722c26
SqrtMinusOne <thexcloud@gmail.com>
2023-12-01 14:20:03 +0300

    Add template

---
+++ org-clock-agg.el
@@ -0,0 +1,34 @@
+;;; org-clock-agg.el --- Aggregate org-clock statistics  -*- lexical-binding: t -*-
+
+;; Copyright (C) 2023 Korytov Pavel
+
+;; Author: Korytov Pavel <thexcloud@gmail.com>
+;; Maintainer: Korytov Pavel <thexcloud@gmail.com>
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "27.1") (compat "29.1.4.1") (org-ql "0.8-pre"))
+;; Homepage: https://github.com/SqrtMinusOne/org-clock-agg
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; TODO
+
+;;; Code:
+(require 'compat)
+
+(provide 'org-clock-agg)
+;;; org-clock-agg.el ends here
